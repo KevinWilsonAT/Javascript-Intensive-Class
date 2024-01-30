93621824771ebaa15c13f073b51112b112f2b37b
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-01-29 22:06:45 -0300

    Audiobook Player Sample Project

---
+++ audiobook/src/scripts/script.js
@@ -0,0 +1,67 @@
+const btnPlayPause = document.getElementById('play-pause');
+const audioChapter = document.getElementById('audio-chapter');
+const btnNext = document.getElementById('forward');
+const btnPrev = document.getElementById('back');
+const chapterName = document.getElementById('chapter');
+const numberChapters = 10;
+let isPlaying = 0;
+let currentChapter = 1;
+
+function playTrack(){
+    audioChapter.play();
+    btnPlayPause.classList.remove('bi-play-circle-fill');
+    btnPlayPause.classList.add('bi-pause-circle-fill');
+}
+
+function pauseTrack(){
+    audioChapter.pause();
+    btnPlayPause.classList.remove('bi-pause-circle-fill');
+    btnPlayPause.classList.add('bi-play-circle-fill');
+}
+
+function playOrPause(){
+    if ( isPlaying === 0 ){
+        playTrack();
+        isPlaying = 1;
+    }
+    else{
+        pauseTrack();
+        isPlaying = 0;
+    }
+}
+
+function swapChapterName(){
+    chapterName.innerText = 'Capítulo ' + currentChapter;
+}
+
+function nextTrack(){
+    if (currentChapter === numberChapters) {
+        currentChapter = 1;
+    }
+    else{
+        currentChapter = currentChapter + 1;
+    }
+
+    audioChapter.src = "./src/media/dom-casmurro/"+currentChapter+".mp3";
+    playTrack();
+    isPlaying = 1;
+    swapChapterName();
+}
+
+function prevTrack(){
+    if (currentChapter === 1) {
+        currentChapter = numberChapters;
+    }
+    else{
+        currentChapter = currentChapter - 1;
+    }
+
+    audioChapter.src = "./src/media/dom-casmurro/"+currentChapter+".mp3";
+    playTrack();
+    isPlaying = 1;
+    swapChapterName();
+}
+
+btnPlayPause.addEventListener('click', playOrPause);
+btnNext.addEventListener('click', nextTrack);
+btnPrev.addEventListener('click', prevTrack);
